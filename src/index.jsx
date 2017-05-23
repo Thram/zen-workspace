219e4e89c53f8ad1671d7c9e7ba4c6841a53c0b8
Thram <thramposo@gmail.com>
2017-05-23 16:31:17 +1200

    - Improve build scripts

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import './styles/main.scss';
 
 render(<App />, document.getElementById('app'));
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,11 @@
  */
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HtmlwebpackPlugin from 'html-webpack-plugin';
-import { optimize } from 'webpack';
+import CompressionPlugin from 'compression-webpack-plugin';
+import ProgressBarPlugin from 'progress-bar-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+import { optimize, LoaderOptionsPlugin, DefinePlugin } from 'webpack';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -21,77 +25,140 @@ const INDEX_HTML_SETUP = {
   inject: false,
 };
 
-export default {
-  cache: true,
-  entry: {
-    app: `${__dirname}/src/index`,
-  },
-  output: {
-    path: `${__dirname}/dist`,
-    filename: '[name].[chunkhash].js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              compact: false,
-              cacheDirectory: true,
-            },
-          },
-        ],
+const plugins = [
+  new ProgressBarPlugin(),
+  new optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    // this assumes your vendor imports exist in the node_modules directory
+    minChunks: module =>
+      module.context && module.context.indexOf('node_modules') !== -1,
+  }),
+  new optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity,
+  }),
+  new optimize.CommonsChunkPlugin({
+    async: true,
+    children: true,
+    minChunks: 4,
+  }),
+  new HtmlwebpackPlugin({
+    ...INDEX_HTML_SETUP,
+    inject: false,
+    chunks: ['vendor', 'manifest', 'app'],
+    filename: `${__dirname}/dist/index.html`,
+  }),
+  new ExtractTextPlugin('styles.css'),
+];
+
+if (isProd) {
+  plugins.push(
+    /**
+     * Options to pass to all loaders
+     */
+    new LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    /**
+     * Minify JS
+     */
+    new optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
       },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: 'file-loader?name=images/[name].[ext]',
+      output: {
+        comments: false,
       },
-      { test: /\.(mp3)$/i, use: 'file-loader?name=audio/[name].[ext]' },
-      { test: /\.(otf)$/i, use: 'file-loader?name=fonts/[name].[ext]' },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+    }),
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
       },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+    }),
+  );
+}
+
+export default function (env = {}) {
+  if (env.compress) {
+    plugins.push(
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|html)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    );
+  }
+  if (env.analyze) plugins.push(new BundleAnalyzerPlugin());
+  return {
+    cache: true,
+    entry: {
+      app: `${__dirname}/src/index`,
+    },
+    output: {
+      path: `${__dirname}/dist`,
+      filename: '[name].[chunkhash].js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: !isProd && 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
           use: [
             {
-              loader: 'css-loader',
-              options: { modules: true, sourceMap: !isProd },
-            },
-            {
-              loader: 'sass-loader',
-              options: { sourceMap: !isProd },
+              loader: 'babel-loader',
+              options: {
+                compact: isProd,
+                cacheDirectory: true,
+              },
             },
           ],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      // this assumes your vendor imports exist in the node_modules directory
-      minChunks: module =>
-        module.context && module.context.indexOf('node_modules') !== -1,
-    }),
-    new HtmlwebpackPlugin({
-      ...INDEX_HTML_SETUP,
-      inject: false,
-      chunks: ['vendor', 'app'],
-      filename: `${__dirname}/dist/index.html`,
-    }),
-    new ExtractTextPlugin('styles.css'),
-  ],
-  performance: {
-    hints: false,
-  },
-};
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: 'file-loader?name=images/[name].[ext]',
+        },
+        { test: /\.(mp3)$/i, use: 'file-loader?name=audio/[name].[ext]' },
+        { test: /\.(otf)$/i, use: 'file-loader?name=fonts/[name].[ext]' },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: { modules: true, sourceMap: !isProd },
+              },
+              {
+                loader: 'sass-loader',
+                options: { sourceMap: !isProd },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    plugins,
+    performance: {
+      hints: false,
+    },
+  };
+}
